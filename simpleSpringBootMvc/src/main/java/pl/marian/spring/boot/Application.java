7a9edda8407f1c17d7f58e593c82f63584d1846b
marians27 <marian.s27@gmail.com>
2016-08-16 21:16:36 +0200

    Simple project using spring boot and spring MVC with jsp

---
+++ simpleSpringBootMvc/src/main/java/pl/marian/spring/boot/Application.java
@@ -0,0 +1,24 @@
+package pl.marian.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ simpleSpringBootMvc/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <b>WELCOME</b>
+        <p>It's working now</p>
+    <body>
+</html>
