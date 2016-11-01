ed566aa3c851ac5c91eb6ad099c1f4ee4a059432
marians27 <marian.s27@gmail.com>
2016-11-01 20:13:20 +0100

    Adding queries

--- spring-data-example/src/main/java/pl/marian/playground/repository/CustomerRepository.java
+++ spring-data-example/src/main/java/pl/marian/playground/repository/CustomerRepository.java
@@ -1,7 +1,10 @@
 package pl.marian.playground.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.marian.playground.domain.Customer;
 
 import java.util.List;
@@ -12,4 +15,18 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     @Query("SELECT c FROM Customer c JOIN FETCH c.accounts a WHERE a.status='A'")
     List<Customer> loadCustomerWithActiveAccount();
+
+    /*
+        WARNING: This is inefficient because hibernate can't create a query with limit if join fetch is used
+        Hence paging will be done in memory, so first all rows will be loaded and then restricted to the proper page.
+     */
+    @Query(value = "SELECT c FROM Customer c LEFT OUTER JOIN FETCH c.accounts a WHERE a.status='A'",
+            countQuery = "SELECT count(*) FROM Customer")
+    Page<Customer> loadCustomerWithActiveAccountPage(Pageable pageable);
+
+    @Query("SELECT c.customerId FROM Customer c")
+    Page<Integer> loadCustomerIdPage(Pageable pageable);
+
+    @Query("SELECT c FROM Customer c LEFT OUTER JOIN FETCH c.accounts a WHERE a.status='A' AND c.customerId IN (:ids)")
+    List<Customer> loadCustomerWithActiveAccountByIds(@Param("ids") List<Integer> ids);
 }
--- spring-data-example/src/test/java/pl/marian/playground/repository/CustomerRepositoryDbUnIT.java
+++ spring-data-example/src/test/java/pl/marian/playground/repository/CustomerRepositoryDbUnIT.java
@@ -2,11 +2,17 @@ package pl.marian.playground.repository;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import pl.marian.playground.domain.Customer;
 import pl.marian.playground.repository.base.SpringDbUnIT;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -53,6 +59,73 @@ public class CustomerRepositoryDbUnIT extends SpringDbUnIT {
         assertThat(customer2.getAccounts()).hasSize(1);
         assertThat(customer2.activeAccount()).isNotNull();
         assertThat(customer2.activeAccount().getDescription()).isEqualTo("A Account for 2");
+    }
+
+    @Test
+    public void loadWithJoinPageable() throws Exception {
+        loadDataSet("pl/marian/playground/repository/customerWithAccounts.dataset.xml");
+
+        Pageable pageRequest = new PageRequest(0, 1, Sort.Direction.ASC, "customerId");
+        Page<Customer> customerPage = customerRepository.loadCustomerWithActiveAccountPage(pageRequest);
+
+        assertThat(customerPage).hasSize(1);
+        Customer customer1 = customerPage.iterator().next();
+        assertThat(customer1.getAccounts()).hasSize(1);
+        assertThat(customer1.activeAccount()).isNotNull();
+        assertThat(customer1.activeAccount().getDescription()).isEqualTo("A Account");
+    }
+
+    @Test
+    public void loadCustomerIdPage() throws Exception {
+        loadDataSet("pl/marian/playground/repository/customerWithAccounts.dataset.xml");
+
+        Pageable pageRequest = new PageRequest(0, 1, Sort.Direction.ASC, "customerId");
+        Page<Integer> idPage = customerRepository.loadCustomerIdPage(pageRequest);
+
+        assertThat(idPage).hasSize(1);
+        assertThat(idPage.getContent().get(0)).isEqualTo(1);
+    }
+
+    @Test
+    public void loadCustomerIdPageTwoElementsPage() throws Exception {
+        loadDataSet("pl/marian/playground/repository/customerWithAccounts.dataset.xml");
+
+        Pageable pageRequest = new PageRequest(0, 2, Sort.Direction.ASC, "customerId");
+        Page<Integer> idPage = customerRepository.loadCustomerIdPage(pageRequest);
+        assertThat(idPage).hasSize(2);
+        assertThat(idPage.getContent().get(0)).isEqualTo(1);
+        assertThat(idPage.getContent().get(1)).isEqualTo(2);
+    }
+
+    @Test
+    public void loadCustomerByIds() throws Exception {
+        loadDataSet("pl/marian/playground/repository/customerWithAccounts.dataset.xml");
+
+        List<Customer> customers = customerRepository.loadCustomerWithActiveAccountByIds(asList(1, 2, 99));
+
+        assertThat(customers).hasSize(2);
+        assertThat(customers.get(0).getCustomerId()).isEqualTo(1);
+        assertThat(customers.get(0).activeAccount().getDescription()).isEqualTo("A Account");
+        assertThat(customers.get(1).getCustomerId()).isEqualTo(2);
+        assertThat(customers.get(1).activeAccount().getDescription()).isEqualTo("A Account for 2");
+    }
+
+    @Test
+    public void loadCustomerByIdsCustomerWithoutActiveAccount() throws Exception {
+        loadDataSet("pl/marian/playground/repository/customerWithAccounts.dataset.xml");
+
+        List<Customer> customers = customerRepository.loadCustomerWithActiveAccountByIds(asList(3, 32));
+
+        assertThat(customers).hasSize(0);
+    }
+
+    @Test
+    public void loadCustomerByIdsEmptyIdsList() throws Exception {
+        loadDataSet("pl/marian/playground/repository/customerWithAccounts.dataset.xml");
+
+        List<Customer> customers = customerRepository.loadCustomerWithActiveAccountByIds(emptyList());
+
+        assertThat(customers).isEmpty();
     }
 
     private Customer findById(List<Customer> customers, int customerId) {
